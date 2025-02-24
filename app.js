ba12529ea4fee35e8357ae6f0f2e063b85cfcbbc
silverraining <www.stella.choi@gmail.com>
2025-02-25 03:08:07 +0900

    cors 설정

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/", router);
 prisma

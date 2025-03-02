3cc07e9618f779fc012bf6dc14f15e3b8901aea4
silverraining <www.stella.choi@gmail.com>
2025-03-03 04:39:24 +0900

    removed /

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://eunb-mission08.netlify.app/",
+    origin: "https://eunb-mission08.netlify.app",
   })
 );
 
